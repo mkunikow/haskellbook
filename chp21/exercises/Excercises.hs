1732531407e5385d511f04c90de89c07397bf560
mkunikow <michal.kunikowski@gmail.com>
2018-06-25 23:47:16 +0200

    chp 21

--- chp21/exercises/Excercises.hs
+++ chp21/exercises/Excercises.hs
@@ -44,9 +44,20 @@ instance Traversable (Constant a) where
     traverse _ Constant {getConstant = x} =  Constant <$> pure x
 
 -- 3. Maybe
-data Optional a = Nada | Yep a
-    
+data Optional a = Nada | Yep a deriving (Eq, Ord, Show)
 
+instance Functor Optional where
+    fmap _ Nada = Nada
+    fmap f (Yep a) = Yep (f a)
+    
+instance Foldable Optional where
+    foldMap _ Nada = mempty
+    foldMap f (Yep a) = f a
+
+
+instance Traversable Optional where
+    traverse f (Yep a) = Yep <$> f a
+    traverse _ Nada =  pure Nada
 
 -- QuickCheck
 
@@ -56,16 +67,24 @@ instance Arbitrary a => Arbitrary (Identity a) where
 instance Arbitrary a => Arbitrary (Constant a b) where
     arbitrary = Constant <$> arbitrary
 
+instance Arbitrary a => Arbitrary (Optional a) where
+    arbitrary = frequency [(1, return Nada), (3, Yep <$> arbitrary)]
+
 instance Eq a => EqProp (Identity a) where
     (=-=) = eq
       
 instance Eq a => EqProp (Constant a b) where
     (=-=) = eq
 
+instance Eq a => EqProp (Optional a) where
+    (=-=) = eq
+
 identityTrigger = undefined :: Identity (Int, Int, [Int])
-constantTrigger = undefined :: Identity (Int, Int, [Int])
+constantTrigger = undefined :: Constant (Int, Int, [Int]) (Int, Int, [Int])
+optionalTrigger = undefined :: Optional (Int, Int, [Int])
 
 main :: IO ()
 main = do
   quickBatch (traversable identityTrigger)
   quickBatch (traversable constantTrigger)
+  quickBatch (traversable optionalTrigger)

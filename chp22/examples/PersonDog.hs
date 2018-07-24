6b0d240a4c6f1b12563899bd9e6741abf229f5e8
mkunikow <michal.kunikowski@gmail.com>
2018-07-25 01:14:55 +0200

    chp22

---
+++ chp22/examples/PersonDog.hs
@@ -0,0 +1,39 @@
+module PersonDog where
+
+newtype HumanName =
+  HumanName String
+  deriving (Eq, Show)
+
+newtype DogName =
+  DogName String
+  deriving (Eq, Show)
+
+newtype Address =
+  Address String
+  deriving (Eq, Show)
+
+data Person = Person
+  { humanName :: HumanName
+  , dogName :: DogName
+  , address :: Address
+  } deriving (Eq, Show)
+
+data Dog = Dog
+  { dogsName :: DogName
+  , dogsAddress :: Address
+  } deriving (Eq, Show)
+
+pers :: Person
+pers =
+  Person (HumanName "Big Bird") (DogName "Barkley") (Address "Sesame Street")
+
+chris :: Person
+chris =   Person (HumanName "Chris Allen") (DogName "Papu") (Address "Austin")
+
+-- without Reader
+getDog :: Person -> Dog
+getDog p = Dog (dogName p) (address p)
+
+-- with Reader
+getDogR :: Person -> Dog
+getDogR = Dog <$> dogName <*> address
---
+++ chp22/exercises/ReadingComprehension.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module ReadingComprehension where
+
+-- 1.    
+myLiftA2 :: Applicative f =>
+            (a -> b -> c)
+         -> f a -> f b -> f c
+myLiftA2 abc fa fb = abc <$> fa <*> fb    
+
+-- 2.
+
+newtype Reader r a = Reader { runReader :: r -> a }
+
+asks :: (r -> a) -> Reader r a
+asks  = Reader
+
+-- 3.
+instance Functor (Reader r) where
+    fmap f (Reader ra) = Reader (f . ra)
+
+instance Applicative (Reader r) where
+    pure :: a -> Reader r a
+    pure a = Reader (const a)
+
+    (<*>) :: Reader r (a -> b)
+        -> Reader r a
+        -> Reader r b
+    (Reader rab) <*> (Reader ra) = Reader (rab <*> ra)
+    -- (Reader rab) <*> (Reader ra) = Reader (\r ->  rab r (ra r))
+
+    -- r -> a ->b
+    -- r -> a
+    -- r -> b
+    
